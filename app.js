db6eb5ee91cd19207ff6e3a342b6e5338cd8731c
tyrant0407 <amankushwaha1331@gmail.com>
2024-04-17 06:25:25 +0530

    BasicBoilerDone

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ require("dotenv").config("path", "./.env");
 const express = require('express')
 const app = express();
 
+//logger
+const logger = require('morgan');
+app.use(logger("tiny"))
 
+//routes
+app.use('/',require('./routes/indexRoutes.js'))
 
 
 app.listen(process.env.PORT,console.log(`server running on ${process.env.PORT}`));
